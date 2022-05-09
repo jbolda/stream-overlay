8b8c5a17734b062a83ea773042fd4f56992f8879
Jacob Bolda <me@jacobbolda.com>
2022-05-09 11:19:24 -0500

    setup base page

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Frame } from "./frame.js";
+
+import "./global.css";
+
+export function App() {
+  return (
+    <React.Fragment>
+      <Frame />
+    </React.Fragment>
+  );
+}
