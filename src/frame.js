665f98012109a5607e5f2740a2cd80784fe8d5cd
Jacob Bolda <me@jacobbolda.com>
2022-05-21 14:44:29 -0500

    hook up twitch events to frame/model

--- src/frame.js
+++ src/frame.js
@@ -1,9 +1,8 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { TransformControls, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 import { useTwitch } from "./context/twitch-inputs";
-import { onEmit } from "effection";
 import { useStream } from "@effection/react";
 
 import * as classes from "./frame.module.css";
@@ -11,10 +10,9 @@ import Model from "./model.js";
 
 export function Frame(props) {
   const twitchClient = useTwitch();
+  let twitchEvent;
   if (twitchClient) {
-    const twitchEvent = useStream(twitchClient);
-
-    console.log(twitchEvent);
+    twitchEvent = useStream(twitchClient);
   }
 
   return (
@@ -22,7 +20,7 @@ export function Frame(props) {
       <ambientLight intensity={1.0} />
       <OrbitControls makeDefault />
       <Suspense fallback={null}>
-        <Model />
+        <Model twitchEvent={twitchEvent} />
       </Suspense>
     </Canvas>
   );
