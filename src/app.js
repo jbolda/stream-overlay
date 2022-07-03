a04eebc6a53902655cbc05fce718bdd24554b76f
Jacob Bolda <me@jacobbolda.com>
2022-07-02 22:32:21 -0500

    implement react-spring with text centering and wrapping

--- src/app.js
+++ src/app.js
@@ -1,16 +1,74 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
+import { useTwitch } from "./context/twitch-inputs";
+import { useOperation } from "@effection/react";
+import { Canvas } from "@react-three/fiber";
+// import { Physics } from "@react-three/cannon";
+
 import { Frame } from "./frame.js";
 import { TextLayer } from "./text-layer.js";
 
 import "./global.css";
+import * as classes from "./text-layer.module.css";
 
 export function App() {
+  const twitchStream = useTwitch();
+  const [twitch3DCommand, setTwitchCommand] = useState();
+  const [twitchRedemption, setTwitchRedemption] = useState();
+  if (twitchStream) {
+    const channelCommands = twitchStream.filter(
+      (twitch) => twitch.event === "onCommand" && twitch.args[1] === "3d"
+    );
+    useOperation(
+      channelCommands.forEach(function* (value) {
+        setTwitchCommand(value);
+      }),
+      [channelCommands]
+    );
+
+    const channelPointRedemption = twitchStream.filter(
+      (twitch) => twitch.event === "onReward"
+    );
+    useOperation(
+      channelPointRedemption.forEach(function* (value) {
+        setTwitchRedemption(value);
+      }),
+      [channelPointRedemption]
+    );
+  }
+
+  const textMessage =
+    twitch3DCommand?.args?.[1].toLowerCase() === "3d"
+      ? twitch3DCommand.args[2]
+      : "";
+  console.log(twitch3DCommand);
+  console.log(twitchRedemption);
+  const rewardMessage = twitchRedemption?.args?.[0]
+    ? `${twitchRedemption?.args?.[0]} redeemed ${twitchRedemption?.args?.[1]}`
+    : "";
+
   return (
-    <div style={{ backgroundColor: "black", height: "100vh" }}>
-      <div style={{ backgroundColor: "white", height: 1080, width: 1920 }}>
-        <TextLayer />
-        {/* <Frame /> */}
-      </div>
+    <div style={{ height: 1080, width: 1920 }}>
+      <Canvas
+        className={classes.canvas}
+        camera={{ fov: 90, position: [0, 0, 5] }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight intensity={0.2} />
+          <directionalLight />
+          <TextLayer {...{ textMessage, setTwitch: setTwitchCommand }} />
+          <TextLayer
+            {...{
+              textMessage: rewardMessage,
+              setTwitch: setTwitchRedemption,
+            }}
+          />
+          {/*
+          <Physics>
+           <Frame />
+          </Physics>
+          */}
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
--- src/text-layer.js
+++ src/text-layer.js
@@ -1,47 +1,74 @@
-import React, { Suspense } from "react";
-import { useTwitch } from "./context/twitch-inputs";
-import { useStream } from "@effection/react";
-import { Canvas } from "@react-three/fiber";
-import { Text3D, Float, Center } from "@react-three/drei";
+import React from "react";
 
-import * as classes from "./text-layer.module.css";
+import { useTransition, animated } from "@react-spring/three";
+import { Text3D, Float, useMatcapTexture } from "@react-three/drei";
+// import { Physics, useBox } from "@react-three/cannon";
 
-export function TextLayer() {
-  const twitchStream = useTwitch();
-  let twitchEvent;
-  if (twitchStream) {
-    twitchEvent = useStream(twitchStream);
-  }
+export const TextLayer = ({ textMessage, setTwitch }) => {
+  // const [meshRef] = useBox(() => ({ mass: 1 }));
 
-  const textMessage =
-    twitchEvent?.event === "onCommand" &&
-    twitchEvent.args[1].toLowerCase() === "3d"
-      ? twitchEvent.args[2]
-      : "";
-  console.log(twitchEvent);
-  return (
-    <Canvas
-      className={classes.canvas}
-      camera={{
-        position: [0, 0.5, 4],
-      }}
-    >
-      <Suspense fallback={null}>
-        <Center>
-          <Float floatIntensity={5} speed={2}>
-            <Text3D
-              font={
-                "https://drei.pmnd.rs/fonts/helvetiker_regular.typeface.json"
-              }
-              bevelEnabled
-              bevelSize={0.05}
-            >
-              {textMessage}
-              <meshNormalMaterial />
-            </Text3D>
-          </Float>
-        </Center>
-      </Suspense>
-    </Canvas>
+  const transition = useTransition(textMessage, {
+    from: { scale: 0, wait: 0 },
+    enter: (item) => async (next, cancel) => {
+      await next({ scale: 1 });
+      await next({ wait: 1 });
+    },
+    leave: { scale: 0, wait: 0 },
+    config: (item, index, phase) => (key) => {
+      return phase === "enter" && key === "wait" ? { duration: 3000 } : {};
+    },
+    onRest: () => setTwitch({}),
+  });
+
+  // we could also look at using these: https://ambientcg.com/
+  const [matcap, url] = useMatcapTexture(
+    "617586_23304C_1B1E30_4988CF"
+    // 0, // index of the matcap texture https://github.com/emmelleppi/matcaps/blob/master/matcap-list.json
+    // 1024 // size of the texture ( 64, 128, 256, 512, 1024 )
   );
-}
+
+  return (
+    <Float floatIntensity={5} speed={2}>
+      {transition(({ scale }, message) => {
+        const wordsArray = message.split(" ");
+        const numberOfWords = wordsArray.length + 1;
+        const wordsPerLine = Math.floor(numberOfWords / 3);
+        const formattedMessage = wordsArray.reduce(
+          (messageArray, word, index) => {
+            const lineIndex = Math.floor(index / wordsPerLine);
+            if (index % wordsPerLine > 0) {
+              messageArray[lineIndex] = `${messageArray[lineIndex]} ${word}`;
+              return messageArray;
+            } else {
+              return messageArray.concat([word]);
+            }
+          },
+          []
+        );
+        return (
+          <animated.mesh scale={scale}>
+            {formattedMessage.map((messageChunk, index) => {
+              const numberOfLetters = messageChunk.split("").length;
+              return (
+                <Text3D
+                  key={index}
+                  font={
+                    "https://drei.pmnd.rs/fonts/helvetiker_regular.typeface.json"
+                  }
+                  size={0.6} // default is 1
+                  height={0.2} // default is 0.2
+                  bevelEnabled
+                  bevelSize={0.05}
+                  position={[-0.5 * (numberOfLetters / 2), -index * 0.9, 0]}
+                >
+                  {messageChunk}
+                  <meshMatcapMaterial matcap={matcap} />
+                </Text3D>
+              );
+            })}
+          </animated.mesh>
+        );
+      })}
+    </Float>
+  );
+};
