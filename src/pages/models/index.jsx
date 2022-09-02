3a99830a753645fb1652027528ac5c3a62136c5d
Jacob Bolda <me@jacobbolda.com>
2022-09-02 13:47:38 -0500

    pull in models from past few streams (#2)
    * add 4 more models from previous streams
    
    * implement star drop
    
    * implement spikey sphere
    
    * implement bump torus
    
    * fix event filters for torus and sphere

--- src/pages/models/index.jsx
+++ src/pages/models/index.jsx
@@ -5,6 +5,10 @@ import { Physics, RigidBody, Debug } from "@react-three/rapier";
 import { Plane } from "@react-three/drei";
 
 import WFlange from "./wflange.jsx";
+// import Cup from "./cup.jsx";
+import Star from "./star.jsx";
+import BumpTorus from "./bump-torus.jsx";
+import SpikeySphere from "./spikey-sphere.jsx";
 
 import * as classes from "../../canvas.module.css";
 
@@ -37,6 +41,10 @@ export default function ModelCanvas() {
             />
           </RigidBody>
           <WFlange count={COUNT} twitchStream={twitchStream} />
+          {/* <Cup count={COUNT} twitchStream={twitchStream} /> */}
+          <Star count={COUNT} twitchStream={twitchStream} />
+          <BumpTorus count={COUNT} twitchStream={twitchStream} />
+          <SpikeySphere count={COUNT} twitchStream={twitchStream} />
         </Physics>
       </Suspense>
     </Canvas>
